f091a2f722eee42cfd1b8eae69f666080925934e
Josh Wolf <josh@wolfs.io>
2024-08-13 05:47:51 -0400

    modify startup scripts to call kubectl directly

--- internal/provider/testdata/pterraform/local-k8s/main.tf
+++ internal/provider/testdata/pterraform/local-k8s/main.tf
@@ -6,11 +6,9 @@ port=$(shuf -i 1024-65535 -n 1)
 docker run --name ${self.id} -l 'dev.chainguard.imagetest=true' -d --privileged -p $port:$port cgr.dev/chainguard/k3s:latest server --disable traefik --disable metrics-server --https-listen-port $port --write-kubeconfig-mode 0644 --tls-san ${self.id} --https-listen-port $port
 
 # Wait for the k3s cluster to be ready
-until docker exec ${self.id} sh -c "k3s kubectl get --raw='/healthz'"; do
+until docker exec ${self.id} sh -c "kubectl get --raw='/healthz'"; do
   sleep 1
 done
-
-# docker exec ${self.id} k3s kubectl config set-cluster default --server https://localhost:$port > /dev/null
 
 docker exec ${self.id} cat /etc/rancher/k3s/k3s.yaml > foo.yaml
 EOF
