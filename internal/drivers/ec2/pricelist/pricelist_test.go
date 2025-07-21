1001b0a784c75b286fc95feb236db119b7ba4810
Illbjorn <am@hades.so>
2025-07-21 08:40:11 -0400

    Clean Up Tests

--- internal/drivers/ec2/pricelist/pricelist_test.go
+++ internal/drivers/ec2/pricelist/pricelist_test.go
@@ -8,40 +8,46 @@ import (
 )
 
 func TestCheapest(t *testing.T) {
+	// This package is a generated price list that changes pretty frequently. To
+	// ensure some consistency with our tests, we've got to game the system a bit
+	// by manually assigning a 'cheapest' instance type.
+	const cheapestPrice float32 = 0.1
+	const cheapestInstanceType = types.InstanceTypeR54xlarge
+	priceList[cheapestInstanceType] = cheapestPrice
 	// Standard case
 	x, pc := SelectCheapest([]types.InstanceType{
-		"c6in.8xlarge",  // 1.8144
-		"r5ad.4xlarge",  // 1.0480
-		"r5d.4xlarge",   // 1.1520
-		"r5.4xlarge",    // 1.0080
-		"c7g.metal",     // 2.3200
-		"c6gn.12xlarge", // 2.0736
-		"r6i.32xlarge",  // 8.0640
-		"cr1.8xlarge",   // 3.5000
-		"r7a.24xlarge",  // 7.3032
-		"p4d.24xlarge",  // 21.9576
-		"c7gn.16xlarge", // 3.9936
+		cheapestInstanceType,
+		"c6in.8xlarge",
+		"r5ad.4xlarge",
+		"r5d.4xlarge",
+		"c7g.metal",
+		"c6gn.12xlarge",
+		"r6i.32xlarge",
+		"cr1.8xlarge",
+		"r7a.24xlarge",
+		"p4d.24xlarge",
+		"c7gn.16xlarge",
 	})
-	assert.Equal(t, types.InstanceTypeR54xlarge, x)
-	assert.Equal(t, float32(1.0080), pc)
+	assert.Equal(t, cheapestInstanceType, x)
+	assert.Equal(t, cheapestPrice, pc)
 
 	// Invalid instance type included
 	x, pc = SelectCheapest([]types.InstanceType{
-		"c6in.8xlarge",         // 1.8144
-		"r5ad.4xlarge",         // 1.0480
-		"r5d.4xlarge",          // 1.1520
-		"r5.4xlarge",           // 1.0080
-		"c7g.metal",            // 2.3200
-		"c6gn.12xlarge",        // 2.0736
-		"r6i.32xlarge",         // 8.0640
-		"cr1.8xlarge",          // 3.5000
-		"r7a.24xlarge",         // 7.3032
-		"p4d.24xlarge",         // 21.9576
-		"c7gn.16xlarge",        // 3.9936
-		"notaninstance.xlarge", // Nope
+		cheapestInstanceType,
+		"c6in.8xlarge",
+		"r5ad.4xlarge",
+		"r5d.4xlarge",
+		"c7g.metal",
+		"c6gn.12xlarge",
+		"r6i.32xlarge",
+		"cr1.8xlarge",
+		"r7a.24xlarge",
+		"p4d.24xlarge",
+		"c7gn.16xlarge",
+		"notaninstance.xlarge",
 	})
-	assert.Equal(t, types.InstanceTypeR54xlarge, x)
-	assert.Equal(t, float32(1.0080), pc)
+	assert.Equal(t, cheapestInstanceType, x)
+	assert.Equal(t, cheapestPrice, pc)
 
 	// Nil input
 	x, pc = SelectCheapest(nil)
