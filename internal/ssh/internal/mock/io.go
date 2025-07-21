a610ab0937193dda14e9cdc72a6b85ab33f3d564
Illbjorn <am@hades.so>
2025-07-21 13:17:58 -0400

    Use a Buffered Reader for SSH Channel Reads

--- internal/ssh/internal/mock/io.go
+++ internal/ssh/internal/mock/io.go
@@ -1,6 +1,7 @@
 package mock
 
 import (
+	"bufio"
 	"io"
 	"testing"
 
@@ -10,20 +11,17 @@ import (
 // asyncRead continuously reads from the provided 'io.Reader', string-converting
 // any read data and passing it through the returned channel.
 func asyncRead(t *testing.T, r io.Reader) <-chan string {
+	br := bufio.NewReader(r)
 	ch := make(chan string, 64)
 	go func(ch chan<- string) {
-		buf := make([]byte, 1024)
 		defer close(ch)
 		for {
-			n, err := r.Read(buf)
+			s, err := br.ReadString('\n')
 			if err != nil {
 				require.ErrorIs(t, err, io.EOF)
 				return
 			}
-			if n == 0 {
-				continue
-			}
-			ch <- string(buf[:n])
+			ch <- string(s)
 		}
 	}(ch)
 	return ch
