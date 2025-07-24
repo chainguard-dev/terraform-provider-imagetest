a1c4683e22571012fc5fa160f958a6ad7087f67f
Illbjorn <am@hades.so>
2025-07-24 09:31:00 -0400

    Drop Filter Package

--- internal/drivers/ec2/filter/post.go
+++
@@ -1,8 +0,0 @@
-package filter
-
-// Post filters are handled in this codebase following a call to
-// 'DescribeInstanceTypes'.
-var Post struct {
-	Storage filtersStoragePost
-	GPU     filtersGPUPost
-}
