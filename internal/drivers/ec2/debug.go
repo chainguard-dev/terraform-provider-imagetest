da384fd2bcebea5d8d7badbfa4d5714ff16b7947
Illbjorn <am@hades.so>
2025-07-29 09:02:50 -0400

    Add `debug` Behavior for EC2 Driver

---
+++ internal/drivers/ec2/debug.go
@@ -0,0 +1,20 @@
+package ec2
+
+import (
+	"os"
+	"strings"
+)
+
+// debugSet indicates whether a specific debug environment variable is enabled.
+//
+// If this environment variable is set, the following measures are taken
+// elsewhere throughout the driver's lifecycle:
+// - For all SSH sessions 'os.Stdout' and 'os.Stderr' will be connected to the
+// session's stdout + stderr streams, rather than the default 'bytes.Buffer'.
+// - When the test container is launched, a separate Goroutine will be spawned
+// which continuously reads the container logs and writes them to stdout.
+func debugSet() bool {
+	const debugVar = "IMAGETEST_EC2_DEBUG"
+	v, ok := os.LookupEnv(debugVar)
+	return ok && (v == "1" || strings.EqualFold(v, "true"))
+}
